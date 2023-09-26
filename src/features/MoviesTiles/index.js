2f032f940259fa8d31f983ba3256ade7c74e611b
Mateusz Grygorowicz <grygorowiczmusic@gmail.com>
2023-09-26 23:18:32 +0200

    moved movieTiles to features, rest to common

---
+++ src/features/MoviesTiles/index.js
@@ -0,0 +1,84 @@
+import { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { selectMoviesList } from "../MovieList/movieListSlice";
+import {
+  MoviesTilesWrapper,
+  ContentLink,
+  MovieTile,
+  MoviePoster,
+  MissingMoviePoster,
+  MissingMoviePosterIcon,
+  MovieInfoContainer,
+  MovieTitle,
+  GenresContainer,
+  RatingContainer,
+  RatingStarIcon,
+  RatingNumber,
+} from "./styled";
+import { SmallGreyText, SmallTile } from "../../common/styled";
+
+const MoviesTiles = () => {
+  const moviesPromise = useSelector((state) => selectMoviesList(state));
+  const [popularMovies, setPopularMovies] = useState([]);
+  const posterUrl = "https://image.tmdb.org/t/p/original";
+
+  useEffect(() => {
+    async function fetchPopularMovies() {
+      try {
+        const movies = await moviesPromise;
+        setPopularMovies(movies.results);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    fetchPopularMovies();
+  }, [moviesPromise]);
+
+  const getReleaseYear = (releaseDate) => {
+    const dateParts = releaseDate.split("-");
+    return dateParts[0];
+  };
+
+  return (
+    <MoviesTilesWrapper>
+      {popularMovies.map((movie) => (
+        <MovieTile key={movie.id}>
+          <ContentLink to={"/movies/:id"}>
+            {movie.poster_path ? (
+              <MoviePoster
+                src={posterUrl + movie.poster_path}
+                alt="Movie Poster"
+              />
+            ) : (
+              <MissingMoviePoster as="div">
+                <MissingMoviePosterIcon />
+              </MissingMoviePoster>
+            )}
+          </ContentLink>
+          <MovieInfoContainer>
+            <ContentLink to={"/movies/:id"}>
+              <MovieTitle>{movie.title}</MovieTitle>
+            </ContentLink>
+            <SmallGreyText>{getReleaseYear(movie.release_date)}</SmallGreyText>
+            <GenresContainer>
+              {/* place to implement .map to generate movie genres with GenreTile */}
+              {/* examples below */}
+              <SmallTile>Action</SmallTile>
+              <SmallTile>Adventure</SmallTile>
+              <SmallTile>Drama</SmallTile>
+            </GenresContainer>
+            <RatingContainer>
+              <RatingStarIcon />
+              <RatingNumber>{movie.vote_average}</RatingNumber>
+              <SmallGreyText>{movie.vote_count} votes</SmallGreyText>
+            </RatingContainer>
+          </MovieInfoContainer>
+        </MovieTile>
+      ))}
+      ;
+    </MoviesTilesWrapper>
+  );
+};
+
+export default MoviesTiles;
---
+++ src/features/MoviesTiles/styled.js
@@ -0,0 +1,140 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ReactComponent as MissingPosterIcon } from "../../images/missingMoviePoster.svg";
+import { ReactComponent as StarIcon } from "../../images/shape-star.svg";
+
+const boxShadowColor = ({ theme }) => theme.color.boxShadow;
+
+export const MoviesTilesWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    grid-template-columns: 1fr;
+    grid-gap: 16px;
+  }
+`;
+
+export const ContentLink = styled(Link)`
+  text-decoration: none;
+`;
+
+export const MovieTile = styled.div`
+  width: 324px;
+  height: 650px;
+  padding: 16px;
+  border-radius: 5px;
+  background-color: ${({ theme }) => theme.breakpoints.mobile};
+  box-shadow: 0px 4px 12px 0px ${boxShadowColor};
+  position: relative;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    min-width: 288px;
+    height: 201px;
+    display: grid;
+    grid-template-columns: max-content 1fr;
+    grid-gap: 16px;
+  }
+`;
+
+export const MoviePoster = styled.img`
+  width: 292px;
+  height: 434px;
+  border-radius: 5px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    width: 114px;
+    height: 169px;
+  }
+`;
+
+export const MissingMoviePoster = styled(MoviePoster)`
+  background-color: ${({ theme }) => theme.color.silver};
+  display: grid;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const MissingMoviePosterIcon = styled(MissingPosterIcon)`
+  width: 120px;
+  height: 120px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    width: 48px;
+    height: 48px;
+  }
+`;
+
+export const MovieInfoContainer = styled.div`
+  height: 100%;
+  margin-top: 16px;
+  display: grid;
+  grid-template-rows: repeat(3, max-content);
+  grid-gap: 8px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    margin-top: 0;
+  }
+`;
+
+export const MovieTitle = styled.h2`
+  color: ${({ theme }) => theme.color.black};
+  margin: 0;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 130%;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 16px;
+  }
+`;
+
+export const GenresContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
+
+export const RatingContainer = styled.div`
+  height: 24px;
+  position: absolute;
+  bottom: 16px;
+  left: 16px;
+  display: grid;
+  grid-template-columns: repeat(3, max-content);
+  place-items: center;
+  grid-gap: 12px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    position: static;
+    height: 16px;
+    grid-gap: 7px;
+  }
+`;
+
+export const RatingStarIcon = styled(StarIcon)`
+  width: 24px;
+  height: 24px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    width: 16px;
+    height: 16px;
+  }
+`;
+
+export const RatingNumber = styled.p`
+  margin: 0;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 150%;
+  text-decoration: none;
+  color: ${({ theme }) => theme.color.black};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 13px;
+    line-height: 130%;
+  }
+`;
