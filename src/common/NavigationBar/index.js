1e901f87ecd27e22c50636d1228c19ea7b6afaec
Maciej Lenartowicz <lenarmathias@gmail.com>
2023-10-04 23:24:18 +0200

    Named the search placeholder based on the path.

--- src/common/NavigationBar/index.js
+++ src/common/NavigationBar/index.js
@@ -1,3 +1,4 @@
+import { useLocation } from "react-router-dom";
 import {
   NavigationBarContainer,
   NavigationBarWrapper,
@@ -14,45 +15,54 @@ import {
   SearchInput,
 } from "./styled";
 
-const NavigationBar = () => (
-  <NavigationBarContainer>
-    <NavigationBarWrapper>
-      <NavigationButtonsWrapper>
-        <HeaderContainer>
-          <LogoNavigationLink
-            to="/movies"
-          >
-            <HeaderLogo />
-            <Heading>
-              Movies Browser
-            </Heading>
-          </LogoNavigationLink>
-        </HeaderContainer>
-        <Navigation>
-          <NavigationItem>
-            <StyledNavigationLink
+const NavigationBar = () => {
+  const location = useLocation();
+
+  return (
+    <NavigationBarContainer>
+      <NavigationBarWrapper>
+        <NavigationButtonsWrapper>
+          <HeaderContainer>
+            <LogoNavigationLink
               to="/movies"
             >
-              Movies
-            </StyledNavigationLink>
-          </NavigationItem>
-          <NavigationItem>
-            <StyledNavigationLink
-              to="/people"
-            >
-              People
-            </StyledNavigationLink>
-          </NavigationItem>
-        </Navigation>
-      </NavigationButtonsWrapper>
-      <SearchContainer>
-        <SearchIcon />
-        <SearchInput
-          placeholder="Search for movies..."
-        />
-      </SearchContainer>
-    </NavigationBarWrapper>
-  </NavigationBarContainer>
-);
+              <HeaderLogo />
+              <Heading>
+                Movies Browser
+              </Heading>
+            </LogoNavigationLink>
+          </HeaderContainer>
+          <Navigation>
+            <NavigationItem>
+              <StyledNavigationLink
+                to="/movies"
+              >
+                Movies
+              </StyledNavigationLink>
+            </NavigationItem>
+            <NavigationItem>
+              <StyledNavigationLink
+                to="/people"
+              >
+                People
+              </StyledNavigationLink>
+            </NavigationItem>
+          </Navigation>
+        </NavigationButtonsWrapper>
+        <SearchContainer>
+          <SearchIcon />
+          <SearchInput
+            placeholder={
+              `Search for ${location.pathname === "/movies"
+                ? "movies..."
+                : "people..."}
+              `
+            }
+          />
+        </SearchContainer>
+      </NavigationBarWrapper>
+    </NavigationBarContainer>
+  )
+};
 
 export default NavigationBar;
